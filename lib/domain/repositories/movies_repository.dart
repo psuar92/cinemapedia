19508999eff8c0b05a38a0db25d0fd9d3f91bd24
Pedro Suarez <psuar92@gmail.com>
2023-12-16 02:28:34 -0600

    Updated repositories

--- lib/domain/repositories/movies_repository.dart
+++ lib/domain/repositories/movies_repository.dart
@@ -1,5 +1,8 @@
 import 'package:cinemapedia/domain/entities/movie.dart';
 
-abstract class MovieRepository {
+abstract class MoviesRepository {
   Future<List<Movie>> getNowPlaying({int page = 1});
+  Future<List<Movie>> getPopular({int page = 1});
+  Future<List<Movie>> getUpcoming({int page = 1});
+  Future<List<Movie>> getTopRated({int page = 1});
 }
