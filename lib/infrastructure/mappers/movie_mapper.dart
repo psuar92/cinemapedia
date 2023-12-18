7cfb3dec90012497431f60b295e4002bf8613a90
Pedro Suarez <psuar92@gmail.com>
2023-12-18 14:37:57 -0600

    Minor updates on mapper

--- lib/infrastructure/mappers/movie_mapper.dart
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -7,7 +7,7 @@ class MovieMapper {
         adult: moviedb.adult,
         backdropPath: (moviedb.backdropPath != '')
             ? 'https://image.tmdb.org/t/p/w500${moviedb.backdropPath}'
-            : 'https://static.displate.com/857x1200/displate/2022-04-15/7422bfe15b3ea7b5933dffd896e9c7f9_46003a1b7353dc7b5a02949bd074432a.jpg',
+            : 'https://www.movienewz.com/img/films/poster-holder.jpg',
         genreIds: moviedb.genreIds.map((e) => e.toString()).toList(),
         id: moviedb.id,
         originalLanguage: moviedb.originalLanguage,
@@ -16,8 +16,9 @@ class MovieMapper {
         popularity: moviedb.popularity,
         posterPath: (moviedb.posterPath != '')
             ? 'https://image.tmdb.org/t/p/w500${moviedb.posterPath}'
-            : 'https://static.displate.com/857x1200/displate/2022-04-15/7422bfe15b3ea7b5933dffd896e9c7f9_46003a1b7353dc7b5a02949bd074432a.jpg',
-        releaseDate: moviedb.releaseDate,
+            : 'https://www.movienewz.com/img/films/poster-holder.jpg',
+        releaseDate:
+            moviedb.releaseDate != null ? moviedb.releaseDate! : DateTime.now(),
         title: moviedb.title,
         video: moviedb.video,
         voteAverage: moviedb.voteAverage,
@@ -28,7 +29,7 @@ class MovieMapper {
         adult: moviedb.adult,
         backdropPath: (moviedb.backdropPath != '')
             ? 'https://image.tmdb.org/t/p/w500${moviedb.backdropPath}'
-            : 'https://static.displate.com/857x1200/displate/2022-04-15/7422bfe15b3ea7b5933dffd896e9c7f9_46003a1b7353dc7b5a02949bd074432a.jpg',
+            : 'https://www.movienewz.com/img/films/poster-holder.jpg',
         genreIds: moviedb.genres.map((e) => e.name).toList(),
         id: moviedb.id,
         originalLanguage: moviedb.originalLanguage,
@@ -37,7 +38,7 @@ class MovieMapper {
         popularity: moviedb.popularity,
         posterPath: (moviedb.posterPath != '')
             ? 'https://image.tmdb.org/t/p/w500${moviedb.posterPath}'
-            : 'https://static.displate.com/857x1200/displate/2022-04-15/7422bfe15b3ea7b5933dffd896e9c7f9_46003a1b7353dc7b5a02949bd074432a.jpg',
+            : 'https://www.movienewz.com/img/films/poster-holder.jpg',
         releaseDate: moviedb.releaseDate,
         title: moviedb.title,
         video: moviedb.video,
