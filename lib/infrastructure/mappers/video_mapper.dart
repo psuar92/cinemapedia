8c5542a13a551200be8be73123be02d69e34925b
Pedro Suarez <psuar92@gmail.com>
2024-01-13 02:47:56 -0600

    Implemented video functionality (not yet showing in movie screen)

---
+++ lib/infrastructure/mappers/video_mapper.dart
@@ -0,0 +1,11 @@
+import 'package:cinemapedia/domain/entities/video.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/moviedb_videos.dart';
+
+class VideoMapper {
+  static moviedbVideoToEntity(Result moviedbVideo) => Video(
+        id: moviedbVideo.id,
+        name: moviedbVideo.name,
+        youtubeKey: moviedbVideo.key,
+        publishedAt: moviedbVideo.publishedAt,
+      );
+}
